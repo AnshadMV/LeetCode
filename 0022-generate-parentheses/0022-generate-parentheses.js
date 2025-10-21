ad0005440f7cd1ad45391afd3659b155d3825220
anshadvelladath <93212393+AnshadMV@users.noreply.github.com>
2025-10-21 09:46:17 +0530

    Time: 0 ms (100%), Space: 56.4 MB (19.15%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    let result = [];
+    function backtrack(open, close, current) {
+        if (open === n && close === n) {
+            result.push(current);
+            return;
+        }
+        if (open < n) {
+            backtrack(open + 1, close, current + "(");
+        }
+        if (close < open) {
+            backtrack(open, close + 1, current + ")");
+        }
+    }
+
+    backtrack(0, 0, "");
+    return result;
+};
