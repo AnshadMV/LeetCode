6830ecebfe84876bb17ac2c47719d97afe57bda0
anshadvelladath <93212393+AnshadMV@users.noreply.github.com>
2025-10-21 09:44:27 +0530

    Time: 11 ms (13.34%), Space: 60.5 MB (65.75%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    let obj = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+    let res=0
+    for (let i = 0; i < s.length; i++) {
+        let current = obj[s[i]]
+        let next = obj[s[i + 1]]
+        if (next > current) {
+            res -= current
+        } else 
+         res  +=current
+    }
+    return res
+};
