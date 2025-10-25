b32cd2ad8ce97fac36767f1649a382466748ba66
anshadvelladath <93212393+AnshadMV@users.noreply.github.com>
2025-10-25 08:29:55 +0530

    Time: 0 ms (100%), Space: 55.5 MB (46.46%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function (x, y, z) {
+    if (Math.abs(z - x) == Math.abs(z - y)) {
+        return 0
+    }
+    else {
+       return Math.abs(z - x) < Math.abs(z - y) ?  1 :  2 ;
+    }
+};
+
+
